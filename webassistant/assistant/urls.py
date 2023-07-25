42630b8b9aabf696e28cf33a13eeb835f6494682
NATASHA AHAMMED <natasha.ahammed@outlook.com>
2023-07-25 11:24:46 -0400

    add path urls for views

---
+++ webassistant/assistant/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# a list of all the urls
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new_chat/', views.new_chat, name='new_chat'),
+    path('error_handler/', views.error_handler, name='error_handler'),
+]
--- webassistant/assistant/views.py
+++ webassistant/assistant/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# Home View
+def home(request):
+    return HttpResponse('The Home Page')
+  
+  
+def new_chat(request):
+  return HttpResponse('New Chat Page')
+
+
+# Error View
+def error_handler(request):
+    return HttpResponse('404 Page')
